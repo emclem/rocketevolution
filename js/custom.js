a5af813fe419ff50ed90c618c91560cbed03f454
emclem <ed_m_clemente@yahoo.com>
2015-09-21 18:08:23 +0800

    Update

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,23 @@
-jQuery(document).ready(function($){
+$(document).ready(function() {
 
 
-
-
-	/************** Scroll Navigation *********************/
-	$('.navigation').singlePageNav({
-        currentClass : 'active'
-    });
-
-
-	/************** FlexSlider *********************/
-    $('.flexslider').flexslider({
-	    animation: "fade",
-	    directionNav: false
+	// RESPONSIVE MENU
+	$('#responsive-btn').click(function() {
+		$('#leftpanel').toggleClass('leftpanel-show');
+		$('#responsive-btn-arrow').toggleClass('fa-chevron-down');
+		$('#responsive-menu').toggleClass('responsive-menu-show');
 	});
 
+	// SLIDESHOW
+    $('#slideshow').cbpFWSlider({speed : 1000, easeing : 'ease'});
+	    setInterval( function() {
+			if($('.cbp-fwnext').is(":visible")) {
+	        	$('.cbp-fwnext').click();
+	        }
+	        else {
+	        	$('.cbp-fwdots').find('span').click();
+	        }
+	}, 4000);
 
-    /************** Responsive Navigation *********************/
 
-	$('.menu-toggle-btn').click(function(){
-        $('.responsive-menu').stop(true,true).slideToggle();
-    });
-
-
-
-
-});
+})
