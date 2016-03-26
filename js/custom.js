dec9fdc9e38a545705395d2cde80d0540b1b0ff8
emclem <ed_m_clemente@yahoo.com>
2016-03-26 18:33:20 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
 
+
+	// SCROLLED
 	$(window).scroll(function() {
 		$('.header').toggleClass('header-scrolled', $(this).scrollTop() > 0);
+	});
+
+
+	// RESPONSIVE MENU
+	$('#responsivebutton').click(function() {
+		$('#responsivemenu').addClass('responsivemenu-clicked');
+	});
+	$('#responsivemenubutton').click(function() {
+		$('#responsivemenu').removeClass('responsivemenu-clicked');
+	});
+
+
+	// BACK TO TOP
+	$('#totop').click(function() {
+		$('html, body').animate({'scrollTop': '0px'}, 1000);
+		return false;
 	});
 
 
@@ -10,10 +28,5 @@ $(document).ready(function() {
 	var RGBChange = function() {
 	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
 	};
-
-	// PRETTY PHOTO
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
 
 });
